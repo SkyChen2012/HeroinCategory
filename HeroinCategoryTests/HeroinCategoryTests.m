1ee01ad703319c76a6fc87b1e6700a7a42033276
Benson Chen <zjxucb@gmail.com>
2018-08-07 13:31:45 +0800

    Creat Heroin Category

---
+++ HeroinCategoryTests/HeroinCategoryTests.m
@@ -0,0 +1,39 @@
+//
+//  HeroinCategoryTests.m
+//  HeroinCategoryTests
+//
+//  Created by Benson on 2018/8/7.
+//  Copyright © 2018年 Benson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HeroinCategoryTests : XCTestCase
+
+@end
+
+@implementation HeroinCategoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
